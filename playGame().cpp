3d243fab33a6f0f8c703c7e4f2fec42af55a34d6
Rooklyn <61075163+rkline2@users.noreply.github.com>
2020-02-17 15:00:10 -0500

    Create playGame().cpp

---
+++ playGame().cpp
@@ -0,0 +1,34 @@
+const int MAXCHAR = 80;
+const int MAXROW = 4;
+const int MAXCOL = 4;
+const int MAXELMNT = 15;
+const char WINNER = 'W';
+const char LOSER = 'L';
+const char YES = 'y';
+const char NO = 'n';
+const char WRONGVAL = '!';
+
+void startGame(int board[MAXROW][MAXCOL]);
+void selectTile(int board[MAXROW][MAXCOL]);
+void swapTile(int board[MAXROW][MAXCOL]);
+bool checkDirection(int board[MAXROW][MAXCOL]);
+void printBoard(int board[MAXROW][MAXCOL]);
+char checkWinner(int board[MAXROW][MAXCOL]);
+void playGame(int board[MAXROW][MAXCOL]);
+
+void playGame(int board[MAXROW][MAXCOL]) {
+    startGame(board);
+    printBoard(board);
+    char isWinner = checkWinner(board);
+    while (isWinner == LOSER) {
+        selectTile(board);
+        if (checkDirection(board)) {
+            swapTile(board);
+        }
+        else {
+            cout << "Incorrect value" << endl;
+        }
+        printBoard(board);
+    }
+    cout << "Winner!" << endl;
+}
