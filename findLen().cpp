c7d0eb693e90f607e142402788011593b3d04d09
Rooklyn <61075163+rkline2@users.noreply.github.com>
2020-02-17 14:43:06 -0500

    Update findLen().cpp

--- findLen().cpp
+++ findLen().cpp
@@ -1,3 +1,13 @@
+const int MAXCHAR = 80;
+const int MAXROW = 4;
+const int MAXCOL = 4;
+const int MAXELMNT = 15;
+const char WINNER = 'W';
+const char LOSER = 'L';
+const char YES = 'y';
+const char NO = 'n';
+const char WRONGVAL = '!';
+
 int findLen(char list[MAXELMNT]) {
     int index = 0;
     while (list[index] != 0) {
