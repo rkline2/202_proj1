a3dc8da280ae06ca1129523b8afe13f4229965dd
Rooklyn <61075163+rkline2@users.noreply.github.com>
2020-02-19 22:00:51 -0500

    Update findLen().cpp

--- findLen().cpp
+++ findLen().cpp
@@ -7,9 +7,10 @@ const char LOSER = 'L';
 const char YES = 'y';
 const char NO = 'n';
 const char WRONGVAL = '!';
+const int DIGIT = 10;
 
 int findLen(char list[MAXELMNT]);
-int findLen(int list[MAXELMNT]);
+int findLen(int testNum);
 
 
 int findLen(char list[MAXELMNT]) {
@@ -20,10 +21,15 @@ int findLen(char list[MAXELMNT]) {
     return index;
 }
 
-int findLen(int list[MAXELMNT]) {
-    int index = 0;
-    while (list[index] != 0) {
-        index++;
+int findLen(int testNum) {
+    int numdigit = 0;
+    if (testNum == 0) {
+        numdigit = 1;
     }
-    return index;
+    while (testNum > 0){
+        testNum /= DIGIT;
+        ++numdigit;
+    }
+   
+    return numdigit;
 }
