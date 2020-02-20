701a0b5802de34a02697b2532e3b16fc810f3419
Rooklyn <61075163+rkline2@users.noreply.github.com>
2020-02-19 22:04:38 -0500

    Create printBoard()-findLen(int).cpp

---
+++ printBoard()-findLen(int).cpp
@@ -0,0 +1,57 @@
+const int MAXCHAR = 80;
+const int MAXROW = 4;
+const int MAXCOL = 4;
+const int MAXELMNT = 15;
+const char WINNER = 'W';
+const char LOSER = 'L';
+const char YES = 'y';
+const char NO = 'n';
+const char WRONGVAL = '!';
+const int DIGIT = 10;
+
+
+void printBoard(int board[MAXROW][MAXCOL]);
+int findLen(int testNum);
+
+
+void printBoard(int board[MAXROW][MAXCOL]){
+    for (int i = 1; i <= 4; i++) {
+        if (i == 1) {
+            cout << "\t" <<"\t" << i << "." << "\t";
+        }
+        else if (i < MAXCOL && i > 1) {
+            cout << "\t" << i << "." << "\t";
+        }
+        else if (i == MAXCOL) {
+            cout << "\t" << i << "." << "\t" << endl;
+        }
+    }
+    for (int row = 0; row < MAXROW; row++) {
+        int col = 0;
+        cout << "\t" << row + 1 << ".";
+        for (int col = 0; col < MAXCOL; col++) {
+            int checkLen = findLen(board[row][col]);
+            if (checkLen == 1) {
+                cout << "\t" << " " << board[row][col] << "|\t";
+            }
+            else {
+                cout << "\t" << board[row][col] << "|\t";
+            }
+        }
+        cout << endl;
+    }
+}
+
+
+int findLen(int testNum) {
+    int numdigit = 0;
+    if (testNum == 0) {
+        numdigit = 1;
+    }
+    while (testNum > 0){
+        testNum /= DIGIT;
+        ++numdigit;
+    }
+   
+    return numdigit;
+}
