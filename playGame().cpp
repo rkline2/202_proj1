80492b93aac76032de9a119be28b1b0c4c8470b2
Rooklyn <61075163+rkline2@users.noreply.github.com>
2020-02-19 22:02:02 -0500

    Update playGame().cpp

--- playGame().cpp
+++ playGame().cpp
@@ -7,6 +7,7 @@ const char LOSER = 'L';
 const char YES = 'y';
 const char NO = 'n';
 const char WRONGVAL = '!';
+const int DIGIT = 10;
 
 void startGame(int board[MAXROW][MAXCOL]);
 void selectTile(int board[MAXROW][MAXCOL]);
