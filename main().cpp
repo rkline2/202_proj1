2455bcb0707b7aed2170771b377c34f4e9cf40d8
Rooklyn <61075163+rkline2@users.noreply.github.com>
2020-02-22 02:09:41 -0500

    Update main().cpp

--- main().cpp
+++ main().cpp
@@ -1,15 +1,35 @@
 
 
+#include <iostream>
+#include <ctime>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
+
+
 const int MAXCHAR = 80;
 const int MAXROW = 4;
 const int MAXCOL = 4;
-const int MAXELMNT = 15;
+const int MAXELMNT = 16;
 const char WINNER = 'W';
 const char LOSER = 'L';
 const char YES = 'y';
 const char NO = 'n';
 const char WRONGVAL = '!';
-const int TENDIGIT = 10;
+const int TWOVAL = 2;
+const char TR_EDGE = '}';
+const char TL_EDGE = '{';
+const char BL_EDGE = '(';
+const char BR_EDGE = ')';
+const char T_SIDE = '^';
+const char B_SIDE = '_';
+const char L_SIDE = '[';
+const char R_SIDE = ']';
+const char CENTER = '-';
+const int ZEROMKR = 0;
+
 
 int main()
 {
