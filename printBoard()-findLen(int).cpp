55581ff7308469baa8d93a074bcb45077d4c7123
Rooklyn <61075163+rkline2@users.noreply.github.com>
2020-02-22 19:31:03 -0500

    Update printBoard()-findLen(int).cpp

--- printBoard()-findLen(int).cpp
+++ printBoard()-findLen(int).cpp
@@ -8,37 +8,111 @@ const char YES = 'y';
 const char NO = 'n';
 const char WRONGVAL = '!';
 const int TENDIGIT = 10;
+const char SPACE = ' ';
+const char H_BAR = '|';
+const char B_BAR = '_';
+const char BAR = '-';
+const char PERIOD = '.';
+const int TENDIGIT = 10;
+const char SPACE = ' ';
+const char H_BAR = '|';
+const char B_BAR = '_';
+const char BAR = '-';
+const char PERIOD = '.';
 
 
 void printBoard(int board[MAXROW][MAXCOL]);
 int findLen(int testNum);
+void addSpaces(int maxspace);
+void createBar(int barLim, char symbol);
+void createBar(int barLim, char symbol, int numSpaces);
 
 
-void printBoard(int board[MAXROW][MAXCOL]){
-    for (int i = 1; i <= 4; i++) {
-        if (i == 1) {
-            cout << "\t" <<"\t" << i << "." << "\t";
+
+void addSpaces(int maxspace) {
+    for (int space = 1; space < maxspace; space++) {
+        cout << SPACE;
+    }
+}
+
+void createBar(int barLim, char symbol) {
+    for (int bar = 1; bar <= barLim; bar++) {
+        if (bar == barLim) {
+            cout << symbol << endl;
+
         }
-        else if (i < MAXCOL && i > 1) {
-            cout << "\t" << i << "." << "\t";
+        else if (bar == 1) {
+            cout << '\t' << symbol;
         }
-        else if (i == MAXCOL) {
-            cout << "\t" << i << "." << "\t" << endl;
+        else {
+            cout << symbol;
         }
     }
+}
+
+void createBar(int barLim, char symbol, int numSpaces) {
+    cout << endl;
+    addSpaces(MAXROW + 4);
+    cout << H_BAR;
+    for (int bar = 1; bar <= barLim; bar++) {
+        if (bar == barLim) {
+            cout << H_BAR << endl;
+
+        }
+        else if (bar % 5 == 0) {
+            cout << H_BAR;
+        }
+        else if (bar == 1) {
+            addSpaces(numSpaces);
+            cout << symbol;
+        }
+        else {
+            cout << symbol;
+        }
+    }
+}
+
+
+
+void printBoard(int board[MAXROW][MAXCOL]) {
+    int underBarLim = (MAXCOL * 3 + MAXCOL * 2);
+    int twoDig = 1;
+    int numSpace = 2;
+    for (int i = 1; i <= MAXCOL; i++) {
+        if (i == 1) {
+            cout << '\t' << SPACE << i << PERIOD;
+        }
+        else if (i < MAXCOL && i > 1) {
+            // add spaces here
+            addSpaces(MAXCOL);
+            cout << i << PERIOD;
+        }
+        else if (i == MAXCOL) {
+            addSpaces(MAXCOL);
+            cout << i << PERIOD << endl;
+        }
+    }
+
+    createBar(underBarLim, B_BAR);
+
     for (int row = 0; row < MAXROW; row++) {
-        int col = 0;
-        cout << "\t" << row + 1 << ".";
+        addSpaces(MAXROW + 2);
+        cout << row + 1 << PERIOD<< H_BAR;
         for (int col = 0; col < MAXCOL; col++) {
             int checkLen = findLen(board[row][col]);
             if (checkLen == 1) {
-                cout << "\t" << " " << board[row][col] << "|\t";
+                // changes here
+                addSpaces(numSpace + 1);
+                cout << board[row][col] << SPACE<< H_BAR;
             }
             else {
-                cout << "\t" << board[row][col] << "|\t";
+                addSpaces(numSpace);
+                cout << board[row][col]  << SPACE << H_BAR;
             }
         }
-        cout << endl;
+        
+        createBar(underBarLim, BAR, 0);
+        
     }
 }
 
@@ -47,10 +121,10 @@ int findLen(int testNum) {
     if (testNum == 0) {
         numdigit = 1;
     }
-    while (testNum > 0){
+    while (testNum > 0) {
         testNum /= TENDIGIT;
         ++numdigit;
     }
-   
+
     return numdigit;
 }
